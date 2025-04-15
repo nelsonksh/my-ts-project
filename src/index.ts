8e50da62fd44c874fdbe7269a66f0b2f297d016d
nelsonksh <nelsonksh@gimbalabs.io>
2025-04-15 22:09:54 +0530

    update tx

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { builtinByteString, conStr0, list, MaestroProvider, MeshTxBuilder, stringToHex, U5CProvider } from "@meshsdk/core";
+import { BlockfrostProvider, builtinByteString, conStr0, list, MaestroProvider, MeshTxBuilder, stringToHex, U5CProvider } from "@meshsdk/core";
+import { MeshWallet } from '@meshsdk/core';
+
+
 
 async function buildTx() {
     // const provider = new U5CProvider({
@@ -14,19 +17,62 @@ async function buildTx() {
         turboSubmit: false
     })
 
+    const blockfrost = new BlockfrostProvider("preprod9nU4kQP5IaqnIFP9M8DhK8bfk1W6dufu")
+
+    const params = await blockfrost.fetchProtocolParameters()
+
+    const wallet = new MeshWallet({
+        networkId: 0, // 0: testnet, 1: mainnet
+        fetcher: maestro,
+        submitter: maestro,
+        key: {
+            type: 'mnemonic',
+            words: [
+                "budget",
+                "fun",
+                "neglect",
+                "expand",
+                "next",
+                "rely",
+                "setup",
+                "matter",
+                "sponsor",
+                "sheriff",
+                "flip",
+                "melt",
+                "deny",
+                "bid",
+                "ankle",
+                "machine",
+                "alley",
+                "they",
+                "never",
+                "oppose",
+                "face",
+                "rely",
+                "pet",
+                "fatigue"
+            ]
+        },
+    });
+
+    const userAddress = await wallet.getChangeAddress()
+    console.log("Wallet address:", userAddress)
+
     const txBuilder = new MeshTxBuilder({
         fetcher: maestro,
         submitter: maestro,
         evaluator: maestro,
+        // params: params,
         verbose: true,
     });
 
     const indexAddress = "addr_test1xrrkcdgg3tyzdj9qu628erlh3kx5f9tcn0rjjsvm8ge5xpvke8x9mpjf7aerjt3n3nfd5tnzkfhlprp09mpf4sdy8dzqs6wq6p"
     const globalStateAddress = "addr_test1xr7xs02kjwr7v3frqrx4exearkd5nmx5ashhzsj5l3nja7yke8x9mpjf7aerjt3n3nfd5tnzkfhlprp09mpf4sdy8dzq6ptcdp"
 
-    const userAddress = "addr_test1qzx4yysnpp9t2692g08gdmvykl22w4fyfqyp2clzrqrx5tdk88srfmghu2rk9nh87744pdaew9x9tz80mqz0pszlhvrs6zlvrg"
 
     const userUtxos = await maestro.fetchAddressUTxOs(userAddress)
+    console.log("userUtxos", JSON.stringify(userUtxos, null, 4))
 
     const policyId = "c76c35088ac826c8a0e6947c8ff78d8d4495789bc729419b3a334305"
 
@@ -34,38 +80,61 @@ async function buildTx() {
     const tokenNameHex = stringToHex(tokenName)
 
     const mintRedeemer = builtinByteString(tokenNameHex)
+    const utxo = await maestro.fetchUTxOs("a4f8080e8e34992977fae292c0f5d843c6c78f837fa69c624e66b6aab868745e", 0)
+    console.log("unit", utxo[0].output.amount[0].unit)
+    console.log("utxo", utxo[0].output.amount[0].quantity)
+
+    console.log("unit", utxo[0].output.amount[1].unit)
+    console.log("utxo", utxo[0].output.amount[1].quantity)
 
     const tx = await txBuilder
         .txInCollateral(userUtxos[0].input.txHash, userUtxos[0].input.outputIndex)
+        // withdrawal
+        .withdrawalPlutusScriptV3()
+        .withdrawal("stake_test17q7dwpfsxsgzdnws8kxn3afatxf4qwl3yhed44vwm5mhexgr3a09v", "0")
+        .withdrawalTxInReference("8a3a9c393bec05d40b73ed459a10a5c9c7a11f197c88d1aaca48080a2e48e7c5", 1)
+        .withdrawalRedeemerValue(conStr0([
+            builtinByteString(tokenNameHex),
+            builtinByteString("20")
+        ]), "JSON")
         // existing index utxo
         .spendingPlutusScriptV3()
-        .txIn("a4f8080e8e34992977fae292c0f5d843c6c78f837fa69c624e66b6aab868745e", 0)
+        .txIn(utxo[0].input.txHash, utxo[0].input.outputIndex)
         .txInInlineDatumPresent()
         .spendingTxInReference("4df3ebc0592b39124c5cc3a1cf680a5d7ac393531dd308e34ee499fbad7257e7", 7)
         .txInRedeemerValue(conStr0([]), "JSON")
         // index token
         .mintPlutusScriptV3()
         .mint("1", policyId, "20")
-        .mintTxInReference("8a3a9c393bec05d40b73ed459a10a5c9c7a11f197c88d1aaca48080a2e48e7c5", 1)
+        .mintTxInReference("4df3ebc0592b39124c5cc3a1cf680a5d7ac393531dd308e34ee499fbad7257e7", 7)
         .mintRedeemerValue(mintRedeemer, "JSON")
         // global state
         .mintPlutusScriptV3()
-        .mint("1", policyId, "313030"+tokenNameHex)
-        .mintTxInReference("8a3a9c393bec05d40b73ed459a10a5c9c7a11f197c88d1aaca48080a2e48e7c5", 1)
+        .mint("1", policyId, "313030" + tokenNameHex)
+        .mintTxInReference("4df3ebc0592b39124c5cc3a1cf680a5d7ac393531dd308e34ee499fbad7257e7", 7)
         .mintRedeemerValue(mintRedeemer, "JSON")
         // user token
         .mintPlutusScriptV3()
-        .mint("1", policyId, "323232"+tokenNameHex)
-        .mintTxInReference("8a3a9c393bec05d40b73ed459a10a5c9c7a11f197c88d1aaca48080a2e48e7c5", 1)
+        .mint("1", policyId, "323232" + tokenNameHex)
+        .mintTxInReference("4df3ebc0592b39124c5cc3a1cf680a5d7ac393531dd308e34ee499fbad7257e7", 7)
         .mintRedeemerValue(mintRedeemer, "JSON")
+        .txOut(
+            "addr_test1qpuwf43fgc6wx3ed20c6wgm267t84ypxdc02qrdnjqkwgtlxakhvwf2dxzsqncufwrrau2ftmv79kh5dl9djq4jly3xspgyfcz",
+            [
+                {
+                    unit: "lovelace",
+                    quantity: "5000000",
+                }
+            ]
+        )
         // index validator
         .txOut(
             indexAddress,
             [
                 {
-                    unit: policyId + "20",
+                    unit: utxo[0].output.amount[1].unit,
                     quantity: "1",
-                }  
+                }
             ]
         )
         .txOutInlineDatumValue(conStr0([
@@ -90,7 +159,7 @@ async function buildTx() {
             globalStateAddress,
             [
                 {
-                    unit: policyId + "313030"+tokenNameHex,
+                    unit: policyId + "313030" + tokenNameHex,
                     quantity: "1",
                 }
             ]
@@ -106,7 +175,7 @@ async function buildTx() {
             userAddress,
             [
                 {
-                    unit: policyId + "323232"+tokenNameHex,
+                    unit: policyId + "323232" + tokenNameHex,
                     quantity: "1",
                 }
             ]
@@ -115,7 +184,10 @@ async function buildTx() {
         .selectUtxosFrom(userUtxos)
         .complete()
 
-    return tx
+    const signedTx = await wallet.signTx(tx)
+    const submittedTx = await wallet.submitTx(signedTx)
+
+    return submittedTx
 
 }
 
@@ -124,9 +196,9 @@ async function buildTx() {
 
 
 buildTx()
-    .then((tx) => {
+    .then((submittedTx) => {
         console.log("Transaction built successfully");
-        console.log("Transaction:", tx);
+        console.log("Transaction:", submittedTx);
     })
     .catch((error) => {
         console.error("Error building transaction:", error);
